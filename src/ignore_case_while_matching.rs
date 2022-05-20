e79b15e8bab6fe34e0fbf953542f9ba2bd77f614
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-05-20 10:23:44 +0100

    Ignore case initial commmit

---
+++ src/ignore_case_while_matching.rs
@@ -0,0 +1,1 @@
+pub fn _ignore_case() {}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod using_the_match_method;
 mod match_literal_strings;
 mod regex_or_operator;
+mod ignore_case_while_matching;
 
 fn main() {
     /* Regular Expressions (RegEx) -
